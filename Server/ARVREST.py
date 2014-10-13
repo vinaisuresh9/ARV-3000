b6c926ffb1618920158c3a2beeef689057c0c7ef
Vinai Suresh <vinaisuresh9@gmail.com>
2014-10-13 11:43:34 -0400

    Some test stuff

--- Server/ARVREST.py
+++ Server/ARVREST.py
@@ -1,26 +1,43 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response, abort
 
 app = Flask(__name__)
 
 tasks = [
          {
          'id': 1,
-         'title': u'Buy groceries',
-         'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+         'title': 'Buy groceries',
+         'description': 'Milk, Cheese, Pizza, Fruit, Tylenol',
          'done': False
          },
          {
          'id': 2,
-         'title': u'Learn Python',
-         'description': u'Need to find a good Python tutorial on the web',
+         'title': 'Learn Python',
+         'description': 'Need to find a good Python tutorial on the web',
+         'done': False
+         },
+         {
+         'id': 3,
+         'title': 'Fake Python',
+         'description': 'Need to find a good BLAHHHH tutorial on the web',
          'done': False
          }
          ]
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@app.route('/api/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
 
+@app.route('/api/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=10080,debug=True)
