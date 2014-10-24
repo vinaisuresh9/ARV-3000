962a33a78a71107943009a95539e3456187db510
Rochelle <rochelle.l.lobo@gmail.com>
2014-10-24 05:25:55 -0400

    Working with Google Maps

---
+++ Android/ARV-3000/src/com/ais/arv_3000/MainActivity.java
@@ -0,0 +1,26 @@
+package com.ais.arv_3000;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		final Button startBtn = (Button) findViewById(R.id.startButton);
+		startBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent startIntent = new Intent(MainActivity.this, MapTextActivity.class);
+				startActivity(startIntent);
+			}
+		});
+	}
+}
---
+++ Android/ARV-3000/src/com/ais/arv_3000/MapTextActivity.java
@@ -0,0 +1,12 @@
+package com.ais.arv_3000;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapTextActivity extends Activity {
+	 @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maptextview);
+    }
+}
