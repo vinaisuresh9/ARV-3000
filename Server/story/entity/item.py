157148afb95cfda8bec384841f7cb7c1ab87bea1
Vinai Suresh <vinaisuresh9@gmail.com>
2014-11-22 12:09:39 -0500

    Small change

--- Server/story/entity/item.py
+++ Server/story/entity/item.py
@@ -1,6 +1,7 @@
 # Not used currently, but might be needed in the future
 class Item:
     def __init__(self, name, desc, location):
+        self.id = id
         self.name = name
         self.desc = desc
         self.location = location
